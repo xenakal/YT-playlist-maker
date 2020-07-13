a7564c79200823617a05f8d75beab1299ba2a4b9
xenakal <alexandros.xenakis@student.uclouvain.be>
2020-07-13 23:23:00 +0200

    program arguments added

--- create_yt_playlist.py
+++ create_yt_playlist.py
@@ -6,21 +6,35 @@ The file must be called "songs.txt" and be located in the same folder as this sc
 # base of the url to create a playlist
 playlist_url = "https://www.youtube.com/watch_videos?video_ids="
 
+# handle input arguments
+import argparse
+parser = argparse.ArgumentParser(description='Make a youtube playlist containing a video for each line of the input file')
+## path 
+parser.add_argument('-p', '--path', default="./songs.txt", help="path to file containing the songs")
+## open browser 
+parser.add_argument('-b', '--use_browser', default=0, help="set to 1 to open playlist in default browser")
+args = parser.parse_args()
+
 # get names/artists of songs 
-filepath = "./songs.txt"
-names_file = open(filepath, 'r')
+names_file = open(args.path, 'r')
 songs_name = names_file.readlines()
 names_file.close()
 
 # search yt for video and append its ID to the playlist url
 from youtubesearchpython import searchYoutube
+print(f"Creating a playlist from the file %s" % args.path)
 for song_name in songs_name:
-    vid_id = searchYoutube(song_name, offset=1, mode="dict", max_results=1).result()["search_result"][0]["id"]
-    playlist_url += vid_id + ","
+    if not song_name[0] == "#":
+        vid_id = searchYoutube(song_name, offset=1, mode="dict", max_results=1).result()["search_result"][0]["id"]
+        playlist_url += vid_id + ","
 playlist_url = playlist_url[:-1]
  
+# output resulting url 
+print(f"Playlist URL: \n \t %s" % playlist_url)
+
 # open the playlist in default browser
-import webbrowser
-webbrowser.open(playlist_url)
+if args.use_browser=="1" or args.use_browser=="T" or args.use_browser=="True" or args.use_browser=="t" or args.use_browser=="true":
+    import webbrowser
+    webbrowser.open(playlist_url)
 
 
