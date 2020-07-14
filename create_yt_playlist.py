4fa8df629d69af19e5c1cf6a72286bded8b51739
xenakal <alexandros.xenakis@student.uclouvain.be>
2020-07-14 17:38:46 +0200

    loading bar added

--- create_yt_playlist.py
+++ create_yt_playlist.py
@@ -19,14 +19,30 @@ args = parser.parse_args()
 names_file = open(args.path, 'r')
 songs_name = names_file.readlines()
 names_file.close()
+vids_number = len([song for song in songs_name if not song[0]=="#"])
+tenth = vids_number/10
 
 # search yt for video and append its ID to the playlist url
+import sys
 from youtubesearchpython import searchYoutube
 print(f"Creating a playlist from the file %s" % args.path)
+number_completed = 0
+sys.stdout.write("[%s]" % (" " * 10))
+sys.stdout.flush()
+sys.stdout.write("\b" * 11)  # return to start of line, after '['
+
 for song_name in songs_name:
     if not song_name[0] == "#":
         vid_id = searchYoutube(song_name, offset=1, mode="dict", max_results=1).result()["search_result"][0]["id"]
         playlist_url += vid_id + ","
+        number_completed += 1
+        while number_completed > tenth:
+           number_completed -= tenth
+           sys.stdout.write("-")
+           sys.stdout.flush()
+
+sys.stdout.write("-\n")
+sys.stdout.flush()
 playlist_url = playlist_url[:-1]
  
 # output resulting url 
@@ -36,5 +52,4 @@ print(f"Playlist URL: \n \t %s" % playlist_url)
 if args.use_browser=="1" or args.use_browser=="T" or args.use_browser=="True" or args.use_browser=="t" or args.use_browser=="true":
     import webbrowser
     webbrowser.open(playlist_url)
-
 
